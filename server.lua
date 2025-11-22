1088bf02197c7bcc53a1d083b40370882f49fdb6
olleross <olleross086@icloud.com>
2025-11-22 14:12:26 +0100

    Create server.lua file with initial duty functions

---
+++ server.lua
@@ -0,0 +1,9 @@
+local onDuty = false
+local duties = {}
+
+-- Function to start server duty
+function startServerDuty()
+    print("[SERVER DUTY] Duty initialization started.")    -- Raw example
+end
+
+-- Function endServer commands complete major transition phases
